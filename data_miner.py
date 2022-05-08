9bfa438609bc0391247eff6d22a5ee5ae0671a35
zakaria <erfan.yekezare@gmail.com>
2022-05-08 22:53:46 +0430

    [get_html] and [get_posts] implementation.

--- data_miner.py
+++ data_miner.py
@@ -1,7 +1,30 @@
+from webbrowser import get
 import requests as req
-re = req.request('GET', 'https://stackoverflow.com/questions/69503329/pip-is-not-working-for-python-3-10-on-ubuntu')
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    re = req.request('GET', url)
+    return re.text
+
+
+def get_posts(raw_html):
+    page_html = BeautifulSoup(raw_html, 'lxml')
+    posts_holder_html = page_html.find('div',class_="flex-col card-holder")
+    posts_html = posts_holder_html.find_all("div")
+    return posts_html
+
+#def get_post():
+#    post_detail = {}
+#    post_detail["icon-location"] = posts_html.f
+#    post_detail["icon-residential"] = 
+#    post_detail["icon-rooms"] = 
+#    post_detail["icon-area"] = 
 #---------------Driver---------------
+
+# finding [  <div _ngcontent-kilid-portal-c153="" class="flex-col card-holder">  ]
 try:
-    print(re.status_code)
+    raw_html = get_html('https://kilid.com/buy/tehran?listingTypeId=1&location=272905&page=2&sort=DATE_DESC')
+    posts_list = get_posts(raw_html)
+    print(posts_list[0].prettify())
 except:
     raise Exception
